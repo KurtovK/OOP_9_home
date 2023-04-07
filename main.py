0b5308454a68d4037fc9b73b0dcb03fd5ffdf5bc
KKD <kossstyan87@mail.ru>
2023-04-07 14:45:45 +0300

    Создал класс Passport

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from typing import Dict, List
 #Задача 1
 #Создайте класс Passport (паспорт), который будет содержать
 #паспортную информацию о гражданине страны. С помощью механизма
 #наследования, реализуйте класс ForeignPassport (загран.паспорт)
 #производный от Passport.
+class Passport:
+    def __init__(self, name : Dict[str, str],gender: str, date_birthday: Dict[str, str], place_birth: str):
+        self.__name = name
+        self.__gender = gender
+        self.__date_brithday = date_birthday
+        self.__place_birth = place_birth
 def execute_application():
     pass
 

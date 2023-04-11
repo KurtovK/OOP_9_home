c1ce2e41ee241ce75a9bb878fe52e4ba8612e526
KKD <kossstyan87@mail.ru>
2023-04-11 11:33:33 +0300

    Создал класс Device

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+#Задача 1
+#Создайте класс Device, который содержит информацию об устройстве.
+#С помощью механизма наследования, реализуйте класс CoffeeMachine
+#(содержит информацию о кофемашине), класс Blender (содержит информацию
+#о блендере).
+class Device:
+    def __init__(self, name: str, brand: str, model: str):
+        self.__name = name
+        self.__brand = brand
+        self.__model = model
+
 def excute_application():
     pass
 if __name__=="__main__":

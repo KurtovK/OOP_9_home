9cd15707bea7a1cac55f887550716997e7a4651e
KKD <kossstyan87@mail.ru>
2023-04-13 16:41:53 +0300

    Объединил классы сделал вывод

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from figure_tools.figures import Square, Ellipse, Circle, Rectangle
 #Задание 1.
 #Создайте базовый класс Shape для хранения плоских фигур.
 #Определите производные классы:
@@ -14,7 +15,43 @@
 
 
 def execute_application():
-    pass
+    print("Фигуры для записи в файлы: \n")
+
+    square = Square(5, 3, 34)
+    square.info()
+
+    circle = Circle(0, 2, 42)
+    circle.info()
+
+    ellipse = Ellipse(7, 1, 12, 78.9)
+    ellipse.info()
+
+    rectangle = Rectangle(9, 9, 5.6, 23.5)
+    rectangle.info()
+
+    figure_list = [square, circle, ellipse, rectangle]
+
+    for num, item in enumerate(figure_list):
+        path = str(num) + "_" + item.__class__.__name__
+        item.write_in_file(path)
+
+    print("Фигуры считанные из файлов:\n")
+
+    path_square = "square.txt"
+    square_from_file = Square.read_from_file(path_square)
+    square_from_file.info()
+
+    path_circle = "circle.txt"
+    circle_from_file = Circle.read_from_file(path_circle)
+    circle_from_file.info()
+
+    path_rectangle = "rectangle.txt"
+    rectangle_from_file = Rectangle.read_from_file(path_rectangle)
+    rectangle_from_file.info()
+
+    path_ellipse = "ellipse.txt"
+    ellipse_from_file = Ellipse.read_from_file(path_ellipse)
+    ellipse_from_file.info()
 
 
 

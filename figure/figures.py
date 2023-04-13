cb30f6718cfbf8ea87523610c60e7256f8d26aae
KKD <kossstyan87@mail.ru>
2023-04-13 16:11:11 +0300

    Добавил info для класса Ellipse

--- figure/figures.py
+++ figure/figures.py
@@ -76,3 +76,7 @@ class Ellipse (__Shape,FigureManagementFile):
     @height.setter
     def height(self, height: float):
         self.__height = height
+    def info(self):
+        print(f"Фигура: {self.NAME}")
+        super().info()
+        print(f"Стороны: {self.__width} x {self.__width}\n")

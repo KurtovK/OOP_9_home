05640307a26bb8f6794803c4f462e179ca848f95
KKD <kossstyan87@mail.ru>
2023-04-13 15:37:30 +0300

    Добавил геттеры и сеттеры в класс Shape

--- figure/figures.py
+++ figure/figures.py
@@ -5,6 +5,19 @@ class Shape:
         self.__x = x
         self.__y = y
 
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, x: int):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, y: int):
+        self.__y = y
 
 class Sguare(Shape, FigureManagementFile):
     pass

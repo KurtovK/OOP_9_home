95469ae03b2ba494acd01a4ff83c383b5f209658
Куртов Константин Дмитриевич <kurtov@yst.local>
2023-04-26 15:25:23 +0300

    Создай class InitializationValueError

--- main.py
+++ main.py
@@ -2,9 +2,17 @@
 #Создайте класс Circle (окружность). Для данного класса реализуйте ряд перегруженных операторов:
 #Проверка на равенство радиусов двух окружностей (операция ==, !=);
 #Проверка сравнения длин двух окружностей (операции >, <, <=,>=);
+class InitializationValueError(Exception):
+    def __init__(self, text):
+        self.__text = text
+
 class Circle:
     def __init__(self, radius: float):
+        if radius <= 0:
+            raise InitializationValueError("Радиус должен быть больше нуля.")
         self.__radius = radius
+        self.__circumference = 2 * pi * self.__radius
+
 
     def __eq__(self, other):
         return self.__radius == other.__radius

8a231a864c0c55a299e3306c40068ed6b2009320
Куртов Константин Дмитриевич <kurtov@yst.local>
2023-04-27 11:59:36 +0300

    Создал класс Flat

--- main.py
+++ main.py
@@ -1,3 +1,47 @@
+#Задание 3.
+#Создайте класс Flat (квартира). Для данного класса реализуйте ряд
+#перегруженных операторов:
+#Проверка на равенство площадей квартир (операция ==);
+#Проверка на неравенство площадей квартир (операция !=);
+#Сравнение двух квартир по стоимости (операции >, <, <=, >=).
+class Flat:
+    def __init__(self, square: float, price: float):
+        self.__square = square
+        self.__price = price
+
+    def __is_flat(self, other):
+        if not isinstance(other, Flat):
+            raise TypeError(f"Сравнение выполнить невозможно "
+                            f"между типом {self.__class__.__name__} "
+                            f"и {other.__class__.__name__}")
+
+    def __eq__(self, other):
+        self.__is_flat(other)
+        return self.__square == other.__square
+
+    def __ne__(self, other):
+        self.__is_flat(other)
+        return self.__square != other.__square
+
+    def __lt__(self, other):
+        self.__is_flat(other)
+        return self.__price < other.__price
+
+    def __gt__(self, other):
+        self.__is_flat(other)
+        return self.__price > other.__price
+
+    def __le__(self, other):
+        self.__is_flat(other)
+        return self.__price <= other.__price
+
+    def __ge__(self, other):
+        self.__is_flat(other)
+        return self.__price >= other.__price
+
+    def __hash__(self):
+        return hash((self.__square, self.__price))
+
 def execute_application():
     pass
 if __name=="__main__":

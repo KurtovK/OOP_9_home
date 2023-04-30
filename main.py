cd3833e729f3d72e61b3db55275c264475ec9139
KKD <kossstyan87@mail.ru>
2023-04-30 20:15:32 +0300

    Внес измения в класс

--- main.py
+++ main.py
@@ -5,46 +5,65 @@
 #Проверка на неравенство площадей квартир (операция !=);
 #Сравнение двух квартир по стоимости (операции >, <, <=, >=).
 class Flat:
-    def __init__(self, square: float, price: float):
-        self.__square = square
-        self.__price = price
+    def __init__(self, square: float, price_per_sq_m: float):
+        self.__validate_square(square)
+        self.__validate_price(price_per_sq_m)
+        self.square = square
+        self.__price_per_sq_m = price_per_sq_m
 
-    def __is_flat(self, other):
+    def __validate_square(self, square: float):
+        if not isinstance(square, (int, float)):
+            raise TypeError("Площадь должна быть числом")
+        if square <= 0:
+            raise ValueError("Площадь должна быть положительной")
+
+    def __validate_price(self, price_per_sq_m: float):
+        if not isinstance(price_per_sq_m, (int, float)):
+            raise TypeError("Цена за квадратный метр должна быть числом")
+        if price_per_sq_m <= 0:
+            raise ValueError("Цена за квадратный метр должна быть положительной")
+
+    def is_flat(self, other):
         if not isinstance(other, Flat):
             raise TypeError(f"Сравнение выполнить невозможно "
                             f"между типом {self.__class__.__name__} "
                             f"и {other.__class__.__name__}")
 
     def __eq__(self, other):
-        self.__is_flat(other)
-        return self.__square == other.__square
+        self.is_flat(other)
+        return self.square == other.square
 
     def __ne__(self, other):
-        self.__is_flat(other)
-        return self.__square != other.__square
+        self.is_flat(other)
+        return self.square != other.square
 
     def __lt__(self, other):
-        self.__is_flat(other)
-        return self.__price < other.__price
+        self.is_flat(other)
+        return self.price < other.price
 
     def __gt__(self, other):
-        self.__is_flat(other)
-        return self.__price > other.__price
+        self.is_flat(other)
+        return self.price > other.price
 
     def __le__(self, other):
-        self.__is_flat(other)
-        return self.__price <= other.__price
+        self.is_flat(other)
+        return self.price <= other.price
 
     def __ge__(self, other):
-        self.__is_flat(other)
-        return self.__price >= other.__price
+        self.is_flat(other)
+        return self.price >= other.price
+
+    @property
+    def price(self):
+        return self.square * self.__price_per_sq_m
 
     def __hash__(self):
-        return hash((self.__square, self.__price))
+        return hash((self.square, self.__price_per_sq_m))
+
 
 def execute_application():
-    f_1 = Flat(60, 1200000)
-    f_2 = Flat(50, 900000)
+    f_1 = Flat(60, 20000)
+    f_2 = Flat(50, 18000)
     try:
         print("Проверка на равно(=):", f_1 == f_2)
         print("Проверка на не равно (!=):", f_1 != f_2)
@@ -54,5 +73,7 @@ def execute_application():
         print("Проверка на больше или равно(>=):", f_1 >= f_2)
     except TypeError as e:
         print(e)
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     execute_application()

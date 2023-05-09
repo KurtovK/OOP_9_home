c6aa5e602e78061ca9c6d63aae3f4af1e5f83931
KKD <kossstyan87@mail.ru>
2023-05-09 16:37:27 +0300

    Добавил класс Clock

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import pickle
+import json
+#Задание 4
+#К уже реализованному классу «Часы» добавьте возможность упаковки и распаковки данных с использованием json и pickle.
+class Clock:
+    def __init__(self, hours=0, minutes=0, seconds=0):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
 def  execute_application():
     pass
 if __name__=="__main__":

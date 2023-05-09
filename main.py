3ec2288f4702b016b66b278b96828ccf0720f891
KKD <kossstyan87@mail.ru>
2023-05-09 16:05:36 +0300

    добавил методы сравнения, упаковки и распаковки данных с использованием json и pickle.

--- main.py
+++ main.py
@@ -6,6 +6,75 @@ class Fraction:
     def __init__(self, numerator, denominator):
         self.numerator = numerator
         self.denominator = denominator
+    def __add__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(self.numerator * other.denominator + other.numerator * self.denominator,
+                            self.denominator * other.denominator)
+        else:
+            raise TypeError("Операция поддерживается только для объектов класса 'Дробь'")
+    def __sub__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(self.numerator * other.denominator - other.numerator * self.denominator,
+                            self.denominator * other.denominator)
+        else:
+            raise TypeError("Операция поддерживается только для объектов класса 'Дробь'")
+    def __mul__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(self.numerator * other.numerator, self.denominator * other.denominator)
+        else:
+            raise TypeError("Операция поддерживается только для объектов класса 'Дробь'")
+    def __truediv__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(self.numerator * other.denominator, self.denominator * other.numerator)
+        else:
+            raise TypeError("Операция поддерживается только для объектов класса 'Дробь'")
+    def __eq__(self, other):
+        if isinstance(other, Fraction):
+            return self.numerator * other.denominator == other.numerator * self.denominator
+        else:
+            return False
+    def __ne__(self, other):
+        if isinstance(other, Fraction):
+            return self.numerator * other.denominator != other.numerator * self.denominator
+        else:
+            return True
+    def __lt__(self, other):
+        if isinstance(other, Fraction):
+            return self.numerator * other.denominator < other.numerator * self.denominator
+        else:
+            raise TypeError("Операция поддерживается только для объектов класса 'Дробь'")
+    def __le__(self, other):
+        if isinstance(other, Fraction):
+            return self.numerator * other.denominator <= other.numerator * self.denominator
+        else:
+            raise TypeError("Операция поддерживается только для объектов класса 'Дробь'")
+    def __gt__(self, other):
+        if isinstance(other, Fraction):
+            return self.numerator * other.denominator > other.numerator * self.denominator
+        else:
+            raise TypeError("Операция поддерживается только для объектов класса 'Дробь'")
+    def __ge__(self, other):
+        if isinstance(other, Fraction):
+            return self.numerator * other.denominator >= other.numerator * self.denominator
+        else:
+            raise TypeError("Операция поддерживается только для объектов класса 'Дробь'")
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+    def pack(self, filename):
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
+    @staticmethod
+    def unpack(filename):
+        with open(filename, "rb") as file:
+            return pickle.load(file)
+    def pack_json(self, filename):
+        with open(filename, "w") as file:
+            json.dump({"numerator": self.numerator, "denominator": self.denominator}, file)
+    @staticmethod
+    def unpack_json(filename):
+        with open(filename, "r") as file:
+            data = json.load(file)
+            return Fraction(data["numerator"], data["denominator"])
 def  execute_application():
     pass
 if __name__=="__main__":

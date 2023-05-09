e373e62e45583f342d3a23181cacc53f14c3f475
KKD <kossstyan87@mail.ru>
2023-05-09 14:03:52 +0300

    Создал класса Airplane

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import pickle
+#Задание 1
+#Создайте класс «Самолет».Наполните его необходимыми характеристиками и методами. Реализуйте упаковку и
+#распаковку объектов класса «Самолет» с использованием модуля pickle.
+class Airplane:
+    def __init__(self, model, manufacturer, max_speed, max_altitude):
+        self.model = model
+        self.manufacturer = manufacturer
+        self.max_speed = max_speed
+        self.max_altitude = max_altitude
+    def __str__(self):
+        return f"Модель: {self.model}\nПроизводитель: {self.manufacturer}\nМаксимальная скорость: {self.max_speed} км/ч\nМаксимальная высота полета: {self.max_altitude} м"
+
+    def to_dict(self):
+        return {"model": self.model, "manufacturer": self.manufacturer, "max_speed": self.max_speed, "max_altitude": self.max_altitude}
+
+
 def  execute_application():
     pass
 if __name__=="__main__":

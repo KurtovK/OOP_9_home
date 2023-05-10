22a651a3698414c254a7685d7c02751ba4ee7df9
Куртов Константин Дмитриевич <kurtov@yst.local>
2023-05-10 08:23:39 +0300

    Создал класс Engine

--- main.py
+++ main.py
@@ -9,19 +9,31 @@ from random import randint
 class Conditioner:
     def __init__(self):
         self.__status = False
-
     @property
     def status_conditioner(self):
         return self.__status
-
     @status_conditioner.setter
     def status_conditioner(self, status: bool):
         self.__status = status
-
     def conditioner_info(self):
         return "Кондиционер " + ("включён." if self.__status else "выключен.")
-
 class StatusConditionerMixin:
     @staticmethod
     def get_status_conditioner(conditioner: Conditioner):
         print(conditioner.conditioner_info())
+
+class Engine:
+    def __init__(self):
+        self.__status_engine = False
+    @property
+    def state(self):
+        return self.__status_engine
+    @state.setter
+    def state(self, status: bool):
+        self.__status_engine = status
+    def engine_info(self):
+        return "Двигатель " + ("заведен." if self.__status_engine else "не заведен.")
+class EngineConditionMixin:
+    @staticmethod
+    def get_status_engine(engine: Engine):
+        print(engine.engine_info())

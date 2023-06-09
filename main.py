166b0c305e1714595b565eee4ce18e8b132e1cef
Куртов Константин Дмитриевич <kurtov@yst.local>
2023-06-09 08:52:32 +0300

    Внес изменения в классы

--- main.py
+++ main.py
@@ -8,39 +8,54 @@
 #Перевод числа в двоичную систему счисления.
 #Перевод числа в десятичную систему счисления.
 class Number:
-    def __init__(self, value, base):
+    def __init__(self, value: str, base: int):
         if base not in [2, 8, 10, 16]:
             raise ValueError("Недопустимая база")
         try:
             int(str(value), base)
         except ValueError:
             raise ValueError("Недопустимый номер для данной базы")
-        self.value = str(value)
-        self.base = base
+        self.__value = value
+        self.__base = base
+    @property
+    def value(self):
+        return self.__value
+
+    @property
+    def base(self):
+        return self.__base
 
 class Calculator:
     @staticmethod
-    def to_octal(number):
+    def to_octal(number: Number) -> Number:
+        if number.base == 8:
+            return number.value
         if number.base != 10:
             number = Number(int(number.value, number.base), 10)
         return oct(int(number.value))[2:]
 
     @staticmethod
-    def to_hexadecimal(number):
+    def to_hexadecimal(number: Number) -> Number:
+        if number.base == 16:
+            return number.value
         if number.base != 10:
             number = Number(int(number.value, number.base), 10)
         return hex(int(number.value))[2:]
 
     @staticmethod
-    def to_binary(number):
+    def to_binary(number: Number) -> Number:
+        if number.base == 2:
+            return number.value
         if number.base != 10:
             number = Number(int(number.value, number.base), 10)
         return bin(int(number.value))[2:]
 
     @staticmethod
-    def to_decimal(number):
+    def to_decimal(number: Number) -> Number:
+        if number.base == 10:
+            return number.value
         return str(int(number.value, number.base))
-def executer_application():
+def execute_application():
     try:
         number1 = Number('1101', 2)
         number2 = Number('ABC', 16)
@@ -57,5 +72,6 @@ def executer_application():
 
     except ValueError as e:
         print(e)
-if __name__ =="__main__":
-    executer_application()
+
+if __name__ == "__main__":
+    execute_application()
